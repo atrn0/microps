a672f8b0504c49c17d3abd321e6d9df3b494bf31
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-08-23 23:21:05 +0900

    Add icmp structs

--- icmp.c
+++ icmp.c
@@ -2,6 +2,109 @@
 #include "ip.h"
 #include "util.h"
 
+// Destination Unreachable Message, Time Exceeded Message, Source Quench Message
+//  0                   1                   2                   3
+//  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  |     Type      |     Code      |          Checksum             |
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  |                             unused                            |
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  |      Internet Header + 64 bits of Original Data Datagram      |
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+// Parameter Problem Message (second line)
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  |    Pointer    |                   unused                      |
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+// Redirect Message (second line)
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  |                 Gateway Internet Address                      |
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+// Echo or Echo Reply Message (from second line)
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  |           Identifier          |        Sequence Number        |
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  |     Data ...
+//  +-+-+-+-+-
+//
+// Timestamp or Timestamp Reply Message (from second line)
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  |           Identifier          |        Sequence Number        |
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  |     Originate Timestamp                                       |
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  |     Receive Timestamp                                         |
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  |     Transmit Timestamp                                        |
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+// Information Request or Information Reply Message
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  |     Type      |      Code     |          Checksum             |
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//  |           Identifier          |        Sequence Number        |
+//  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+
+struct icmp_hdr {
+  uint8_t type;
+  uint8_t code;
+  uint16_t sum;
+  uint32_t values;
+};
+
+struct icmp_echo {
+  uint8_t type;
+  uint8_t code;
+  uint16_t sum;
+  uint16_t id;
+  uint16_t seq;
+};
+
+static char *icmp_type_ntoa(uint8_t type) {
+  switch (type) {
+    case ICMP_TYPE_ECHOREPLY:return "EchoReply";
+    case ICMP_TYPE_DEST_UNREACH:return "DestinationUnreachable";
+    case ICMP_TYPE_SOURCE_QUENCH:return "SourceQuench";
+    case ICMP_TYPE_REDIRECT:return "Redirect";
+    case ICMP_TYPE_ECHO:return "Echo";
+    case ICMP_TYPE_TIME_EXCEEDED:return "TimeExceeded";
+    case ICMP_TYPE_PARAM_PROBLEM:return "ParameterProblem";
+    case ICMP_TYPE_TIMESTAMP:return "Timestamp";
+    case ICMP_TYPE_TIMESTAMPREPLY:return "TimestampReply";
+    case ICMP_TYPE_INFO_REQUEST:return "InformationRequest";
+    case ICMP_TYPE_INFO_REPLY:return "InformationReply";
+  }
+  return "Unknown";
+}
+
+static void icmp_dump(const uint8_t *data, size_t len) {
+  struct icmp_hdr *hdr;
+  struct icmp_echo *echo;
+
+  flockfile(stderr);
+  hdr = (struct icmp_hdr *) data;
+  fprintf(stderr, "       type: %u (%s)\n", hdr->type, icmp_type_ntoa(hdr->type));
+  fprintf(stderr, "       code: %u\n", hdr->code);
+  fprintf(stderr, "        sum: 0x%04x\n", ntoh16(hdr->sum));
+  switch (hdr->type) {
+    case ICMP_TYPE_ECHOREPLY:
+    case ICMP_TYPE_ECHO:echo = (struct icmp_echo *) hdr;
+      fprintf(stderr, "         id: %u\n", ntoh16(echo->id));
+      fprintf(stderr, "        seq: %u\n", ntoh16(echo->seq));
+      break;
+    default:fprintf(stderr, "     values: 0x%08x\n", ntoh32(hdr->values));
+      break;
+  }
+#ifdef HEXDUMP
+  hexdump(stderr, data, len);
+#endif
+  funlockfile(stderr);
+}
+
 void icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface) {
   char addr1[IP_ADDR_STR_LEN];
   char addr2[IP_ADDR_STR_LEN];
