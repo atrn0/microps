795e47cd8895ebf75bcd19fc1def5c8d13e00e7b
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-08-23 23:34:11 +0900

    validate icmp header

--- icmp.c
+++ icmp.c
@@ -109,8 +109,20 @@ void icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, s
   char addr1[IP_ADDR_STR_LEN];
   char addr2[IP_ADDR_STR_LEN];
 
+  if (len < ICMP_HDR_SIZE) {
+    errorf("input data is too short: %d bytes", len);
+    return;
+  }
+
+  // checksum
+  uint16_t sum = cksum16((uint16_t *) data, len, 0);
+  if (sum != 0) {
+    errorf("checksum not match: 0x%02x", sum);
+    return;
+  }
+
   debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)), ip_addr_ntop(dst, addr2, sizeof(addr2)), len);
-  debugdump(data, len);
+  icmp_dump(data, len);
 }
 
 int icmp_init() {
