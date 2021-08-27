a7315dd5bc46b48d3c00f291c54f9e51905df4b0
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-08-27 21:25:05 +0900

    Add test

---
+++ test/step11.c
@@ -0,0 +1,79 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <icmp.h>
+
+#include "driver/loopback.h"
+#include "ip.h"
+#include "net.h"
+#include "test.h"
+#include "util.h"
+#include "driver/ether_tap.h"
+
+static volatile sig_atomic_t terminate;
+
+static void on_signal(int s) {
+  (void) s;
+  terminate = 1;
+}
+
+static int setup() {
+  if (net_init() == -1) {
+    errorf("net_init() failure");
+    return -1;
+  }
+
+  struct net_device *dev = loopback_init();
+  if (!dev) {
+    errorf("loopback_init() failure");
+    return -1;
+  }
+
+  struct ip_iface *iface = ip_iface_alloc(LOOPBACK_IP_ADDR, LOOPBACK_NETMASK);
+  if (!iface) {
+    errorf("ip_iface_alloc() failure");
+    return -1;
+  }
+  if (ip_iface_register(dev, iface) == -1) {
+    errorf("ip_iface_register() failure");
+    return -1;
+  }
+
+  struct net_device *dev2 = ether_tap_init(ETHER_TAP_NAME, ETHER_TAP_HW_ADDR);
+  if (!dev2) {
+    errorf("ether_tap_init() failure");
+    return -1;
+  }
+  struct ip_iface *iface2 = ip_iface_alloc(ETHER_TAP_IP_ADDR, ETHER_TAP_NETMASK);
+  if (!iface2) {
+    errorf("ip_iface_alloc() failure");
+    return -1;
+  }
+  if (ip_iface_register(dev2, iface2) == -1) {
+    errorf("ip_iface_register() failure");
+    return -1;
+  }
+
+  if (net_run() == -1) {
+    errorf("net_run() failure");
+    return -1;
+  }
+
+  return 0;
+}
+
+static void cleanup() {
+  net_shutdown();
+}
+
+int main(int argc, char *argv[]) {
+  signal(SIGINT, on_signal);
+  if (setup() == -1) {
+    errorf("setup() failure");
+    return -1;
+  }
+  while (!terminate) {
+    sleep(1);
+  }
+  cleanup();
+}
