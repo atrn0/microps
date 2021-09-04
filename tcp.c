4ab3f24472ab522a84dfc2b1c83cd42956a22d67
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-09-04 15:15:02 +0900

    TCP struct and dump

---
+++ tcp.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "util.h"
+#include "ip.h"
+#include "tcp.h"
+
+#define TCP_FLG_FIN 0x01
+#define TCP_FLG_SYN 0x02
+#define TCP_FLG_RST 0x04
+#define TCP_FLG_PSH 0x08
+#define TCP_FLG_ACK 0x10
+#define TCP_FLG_URG 0x20
+
+#define TCP_FLG_IS(x, y) ((x & 0x3f) == (y))
+#define TCP_FLG_ISSET(x, y) ((x & 0x3f) & (y) ? 1 : 0)
+
+struct pseudo_hdr {
+  uint32_t src;
+  uint32_t dst;
+  uint8_t zero;
+  uint8_t protocol;
+  uint16_t len;
+};
+
+struct tcp_hdr {
+  uint16_t src;
+  uint16_t dst;
+  uint32_t seq;
+  uint32_t ack;
+  uint8_t off;
+  uint8_t flg;
+  uint16_t wnd;
+  uint16_t sum;
+  uint16_t up;
+};
+
+int tcp_endpoint_pton(char *p, struct tcp_endpoint *n) {
+  char *sep;
+  char addr[IP_ADDR_STR_LEN] = {};
+  long int port;
+
+  sep = strrchr(p, ':');
+  if (!sep) {
+    return -1;
+  }
+  memcpy(addr, p, sep - p);
+  if (ip_addr_pton(addr, &n->addr) == -1) {
+    return -1;
+  }
+  port = strtol(sep + 1, NULL, 10);
+  if (port <= 0 || port > UINT16_MAX) {
+    return -1;
+  }
+  n->port = hton16(port);
+  return 0;
+}
+
+char *tcp_endpoint_ntop(struct tcp_endpoint *n, char *p, size_t size) {
+  size_t offset;
+
+  ip_addr_ntop(n->addr, p, size);
+  offset = strlen(p);
+  snprintf(p + offset, size - offset, ":%d", ntoh16(n->port));
+  return p;
+}
+
+static char *tcp_flg_ntoa(uint8_t flg) {
+  static char str[9];
+
+  snprintf(str, sizeof(str), "--%c%c%c%c%c%c",
+           TCP_FLG_ISSET(flg, TCP_FLG_URG) ? 'U' : '-',
+           TCP_FLG_ISSET(flg, TCP_FLG_ACK) ? 'A' : '-',
+           TCP_FLG_ISSET(flg, TCP_FLG_PSH) ? 'P' : '-',
+           TCP_FLG_ISSET(flg, TCP_FLG_RST) ? 'R' : '-',
+           TCP_FLG_ISSET(flg, TCP_FLG_SYN) ? 'S' : '-',
+           TCP_FLG_ISSET(flg, TCP_FLG_FIN) ? 'F' : '-');
+  return str;
+}
+
+static void tcp_dump(const uint8_t *data, size_t len) {
+  struct tcp_hdr *hdr;
+
+  flockfile(stderr);
+  hdr = (struct tcp_hdr *) data;
+  fprintf(stderr, "        src: %u\n", ntoh16(hdr->src));
+  fprintf(stderr, "        dst: %u\n", ntoh16(hdr->dst));
+  fprintf(stderr, "        seq: %u\n", ntoh32(hdr->seq));
+  fprintf(stderr, "        ack: %u\n", ntoh32(hdr->ack));
+  fprintf(stderr, "        off: 0x%02x (%d)\n", hdr->off, (hdr->off >> 4) << 2);
+  fprintf(stderr, "        flg: 0x%02x (%s)\n", hdr->flg, tcp_flg_ntoa(hdr->flg));
+  fprintf(stderr, "        wnd: %u\n", ntoh16(hdr->wnd));
+  fprintf(stderr, "        sum: 0x%04x\n", ntoh16(hdr->sum));
+  fprintf(stderr, "         up: %u\n", ntoh16(hdr->up));
+#ifdef HEXDUMP
+  hexdump(stderr, data, len);
+#endif
+  funlockfile(stderr);
+
+}
+
+static void tcp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface) {
+
+}
+
+int tcp_init(void) {
+
+}
