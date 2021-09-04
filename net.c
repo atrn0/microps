269cdd76cd2b31f780c15aebac1d9c30ebabb7dd
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-09-04 16:22:43 +0900

    input TCP datagram

--- net.c
+++ net.c
@@ -14,6 +14,7 @@
 #include "icmp.h"
 #include "arp.h"
 #include "udp.h"
+#include "tcp.h"
 
 struct net_protocol {
   struct net_protocol *next;
@@ -426,6 +427,11 @@ int net_init(void) {
 
   if (udp_init() == -1) {
     errorf("udp_init() failure");
+    return -1;
+  }
+
+  if (tcp_init() == -1) {
+    errorf("tcp_init() failure");
     return -1;
   }
 
